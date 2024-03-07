1856d3478265a5459d26f044b6316e5ad6425585
kart9071 <karthikakala13@gmail.com>
2024-03-07 20:45:55 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+declare const angular: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  openDropdown(event:MouseEvent):void{
+    event?.preventDefault();
+    const dropdown=(event.target as HTMLElement).nextElementSibling; // Get the next sibling element (the dropdown menu)
+    if (dropdown) {
+      dropdown.classList.toggle('show'); // Toggle the 'show' class to display the dropdown menu
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewPageComponent } from './new-page/new-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPageComponent
   ],
   imports: [
     BrowserModule
